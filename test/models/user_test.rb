f33727cac6afaf47f5f2c46c73830d7244b253b0
androidgrl <kawaharajamie@gmail.com>
2015-05-02 23:38:57 -0600

    adding secure password and minimum length requirement

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com", password: "eggsntoast", password_confirmation: "eggsntoast")
   end
 
   test "should be valid" do
@@ -52,5 +52,10 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
