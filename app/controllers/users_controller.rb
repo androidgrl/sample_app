da8b0524f36b71e5d1453245d46b0e6ba0e1c3f8
androidgrl <kawaharajamie@gmail.com>
2015-05-08 15:30:09 -0600

    Moving logged_in_user to application controller and putting in before_action filter in microposts controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,8 @@
 class UsersController < ApplicationController
-
   before_action :logged_in_user,  only: [:edit, :update, :index, :destroy]
   before_action :correct_user,    only: [:edit, :update]
   before_action :admin_user,      only: :destroy
+
   def index
     @users = User.paginate(page: params[:page])
   end
@@ -51,15 +51,7 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password, :password_confirmation)
     end
 
-    def logged_in_user
-      unless logged_in?
-        store_location
-        flash[:danger] = "Please log in"
-        redirect_to login_url
-      end
-    end
-
-    def correct_user
+     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
     end
